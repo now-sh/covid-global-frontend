90e733db311bc65c0f3725b699a68cf98e9a6eed
casjay <git-admin@casjaysdev.pro>
2025-09-05 14:04:26 -0400

    📃 Deployed: src/main.js 📃
    Deployed: src/main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/dracula-theme.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 import App from './App.vue'
